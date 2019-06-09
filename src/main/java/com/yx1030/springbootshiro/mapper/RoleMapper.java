5012a42088c55f7047f20f29285b6b1b86e7416d
99732 <997323529@qq.com>
2019-06-09 08:43:42 +0800

    test

---
+++ src/main/java/com/yx1030/springbootshiro/mapper/RoleMapper.java
@@ -0,0 +1,17 @@
+package com.yx1030.springbootshiro.mapper;
+
+import com.yx1030.springbootshiro.pojo.Role;
+
+public interface RoleMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Role record);
+
+    int insertSelective(Role record);
+
+    Role selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Role record);
+
+    int updateByPrimaryKey(Role record);
+}
---
+++ src/main/java/com/yx1030/springbootshiro/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.yx1030.springbootshiro.mapper;
+
+import com.yx1030.springbootshiro.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
